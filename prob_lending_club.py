0a34aadba5ee307ed03c2d058685c1b864888394
cdorman <cdorman@jumptrading.com>
2016-05-02 18:58:29 -0500

    adding code for lesson

---
+++ prob_lending_club.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+
+import pandas as pd
+
+loansData = pd.read_csv('/home/cdorman/PycharmProjects/Thinkful/Data_Science/lending_club.csv')
+
+loansData.dropna(inplace=True)
+
+loansData.boxplot(column='Amount.Funded.By.Investors', return_type='dict')
+
+plt.show()
