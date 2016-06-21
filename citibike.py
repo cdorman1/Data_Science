959993893937842fca9822a00fdf3752be860d55
cdorman1 <cdorman09@gmail.com>
2016-06-20 23:04:56 -0500

    citibike: adding new code

--- citibike.py
+++ citibike.py
@@ -1,36 +1,28 @@
-# New Yorkers Bike
 import time
-import requests
 from dateutil.parser import parse
 import collections
-import sqlite3 as lite
 import pandas as pd
+import sqlite3 as lite
 import matplotlib.pyplot as plt
+import requests
+import datetime
 
 con = lite.connect('citi_bike.db')
 cur = con.cursor()
 
 for i in range(60):
     r = requests.get('http://www.citibikenyc.com/stations/json')
-    exec_time = parse(r.json()['executionTime'])
-    print exec_time
+    exec_time = parse(r.json()['executionTime']).strftime("%s")
 
+    cur.execute('INSERT INTO available_bikes (execution_time) VALUES (?)', (exec_time,))
 
-    cur.execute('INSERT INTO available_bikes (execution_time) VALUES (?)', (exec_time.strftime('%c'),))
-    con.commit()
-
-    id_bikes = collections.defaultdict(int)
     for station in r.json()['stationBeanList']:
-        id_bikes[station['id']] = station['availableBikes']
-
-    for k, v in id_bikes.iteritems():
-        cur.execute(
-            "UPDATE available_bikes SET _" + str(k) + " = " + str(v) + " WHERE execution_time = '" + exec_time.strftime(
-                '%c') + "';")
+        cur.execute("UPDATE available_bikes SET _%d = %d WHERE execution_time = %s" % (station['id'], station['availableBikes'], exec_time))
     con.commit()
 
     time.sleep(60)
-con.close()
+
+con.close() #close the database connection when done
 
 
 ### Analysis
@@ -38,35 +30,38 @@ con.close()
 con = lite.connect('citi_bike.db')
 cur = con.cursor()
 
-hour_data = pd.read_sql_query("SELECT * FROM available_bikes WHERE execution_time like 'Thu Jun 16%' ORDER BY execution_time", con, index_col='execution_time')
+df = pd.read_sql_query("SELECT * FROM available_bikes ORDER BY execution_time",con,index_col='execution_time')
 
-hour_data = hour_data.dropna()
+
+hour_data = df.dropna()
+
 hour_change = collections.defaultdict(int)
-
-for col in hour_data.columns:
-    station_vals = hour_data[col].tolist()
-    station_id = col[1:]  # trim the "_"\
+for col in df.columns:
+    station_vals = df[col].tolist()
+    station_id = col[1:] #trim the "_"
     station_change = 0
-
-    for k, v in enumerate(station_vals):
+    for k,v in enumerate(station_vals):
         if k < len(station_vals) - 1:
-            station_change += abs(station_vals[k] - station_vals[k + 1])
-    hour_change[int(station_id)] = station_change
+            station_change += abs(station_vals[k] - station_vals[k+1])
+    hour_change[int(station_id)] = station_change #convert the station id back to integer
 
 
 def keywithmaxval(d):
-    v = list(d.values())
-    k = list(d.keys())
-    return k[v.index(max(v))]
+    """Find the key with the greatest value"""
+    return max(d, key=lambda k: d[k])
 
+# assign the max key to max_station
 max_station = keywithmaxval(hour_change)
 
+#query sqlite for reference information
 cur.execute("SELECT id, stationname, latitude, longitude FROM citibike_reference WHERE id = ?", (max_station,))
+data = cur.fetchone()
+print("The most active station is station id %s at %s latitude: %s longitude: %s " % data)
+print("With %d bicycles coming and going in the hour between %s and %s" % (
+    hour_change[max_station],
+    datetime.datetime.fromtimestamp(int(df.index[0])).strftime('%Y-%m-%dT%H:%M:%S'),
+    datetime.datetime.fromtimestamp(int(df.index[-1])).strftime('%Y-%m-%dT%H:%M:%S'),
+))
 
-data_max_station = cur.fetchone()
-con.close()
-
-print "The most active station is station id %s at %s latitude: %s longitude: %s " % data_max_station
-print "With " + str(hour_change[max_station]) + " bicycles coming and going in the hour between " + str(hour_data.index[0]) + " and " + str(hour_data.index[-1])
 plt.bar(hour_change.keys(), hour_change.values())
 plt.show()
