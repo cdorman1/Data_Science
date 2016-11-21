1ae27561fbcb3f8a0b2b5e31e7b8c1da93778128
cdorman1 <cdorman09@gmail.com>
2016-11-21 14:56:44 -0600

    capstone project: naive bayes

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,13 @@
-print("Hello World I'm now becoming a Git Ninja!!!")
+# print("Hello World I'm now becoming a Git Ninja!!!")
+
+
+
+self.logger.info('New Hotstock: {} trade_ratio={},'
+                 'bid=${:.2f}, ask=${:.2f},'
+                 'close_price={:.2f},'
+                 'pct_chng={:.2f}'.format(hot_sym,
+                                          trade_ratio,
+                                          bid,
+                                          ask,
+                                          close_price,
+                                          pct_chng))
